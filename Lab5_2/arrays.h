fbe0cc9e66c2d1aa876a46f1a024bb87890a6fb0
Buivid <sbujvid@gmail.com>
2023-02-12 18:42:13 +0300

    ОАиП

--- Lab5_2/arrays.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-void memory(int*** mas, int row, int k);
-void output_matrix(int** mas, int row);
-void delete_neg_elements(int** mas, int size);
-int check_number_of_elements();
-void input_row(int** mas, int row);
--- Lab8/functions.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-int len(char* str);
-int size_min_word(char* str);
-void sort(char** mas, int size);
-void output_array(char** mas, int size);
